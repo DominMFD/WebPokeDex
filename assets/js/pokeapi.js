000dfa40d4e56e2e80e1cb56e91daf4f1ad5d98a
DominMFD <134434652+DominMFD@users.noreply.github.com>
2023-08-23 15:08:14 -0300

    Update pokeapi.js

--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -35,12 +35,3 @@ pokeApi.getPokemons =  (offset = 0, limit = 10) => {
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonDetails) => pokemonDetails)
 }
-
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4')
-]).then((detailsPoke) => {
-    console.log(detailsPoke)
-})
